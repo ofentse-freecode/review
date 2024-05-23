25a7f85e3908b7b20b65a2e068bbfc329e57ed74
Ofentse Langa <ofentsel.work@gmail.com>
2024-05-23 17:38:49 +0200

    final

---
+++ review-card/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './index.css';
+import Review from './Review';
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
+ 
